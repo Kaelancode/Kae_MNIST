223b4fd54ab606015939f3ba4ce4d2e0bf635b03
Kaelan Sun <kaelan@my.mail.de>
2023-01-05 03:49:29 +0800

    Update settings.py

--- digit_classifier/settings.py
+++ digit_classifier/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('PROJECT_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'kaemnist.onrender.com']
 
 
 # Application definition
